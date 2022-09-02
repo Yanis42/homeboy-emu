4bae579d0a41c48f7d372929887edf35b3dcbc9f
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-09-02 19:03:30 +0200

    rom split

---
+++ src/rom.c
@@ -0,0 +1,81 @@
+#include "types.h"
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romEvent.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetImage.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romSetImage.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romSetCacheSize.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romUpdate.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romCopyImmediate.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romCopy.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetDebug64.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetDebug32.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetDebug16.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetDebug8.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPutDebug64.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPutDebug32.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPutDebug16.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPutDebug8.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGet64.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGet32.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGet16.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGet8.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPut64.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPut32.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPut16.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romPut8.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romTestCode.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetCode.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romGetPC.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romLoadFullOrPart.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/__romCopyUpdate_Complete.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romLoadUpdate.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/__romLoadUpdate_Complete.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romCacheGame.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romCacheGame_ZELDA.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romLoadRange.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romLoadBlock.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/__romLoadBlock_CompleteGCN.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/__romLoadBlock_Complete.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romSetBlockCache.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romMakeFreeCache.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romFindOldestBlock.s")
+
+#pragma GLOBAL_ASM("asm/non_matching/rom/romFindFreeCache.s")
